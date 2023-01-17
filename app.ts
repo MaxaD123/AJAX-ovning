19bfc6aeccf058911f5417f5779987c9240ba945
Max Taylor <maxtaylor0804@gmail.com>
2023-01-17 20:28:32 +0100

    första commit

---
+++ app.ts
@@ -0,0 +1,48 @@
+const url = "https://dog.ceo/api/breeds/image/random"
+const dogPic = document.querySelector(".dogPic") as HTMLElement;
+
+let button = document.querySelector(".newDog") as HTMLElement;
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+    dogPic.innerHTML = "";
+    
+    
+    const k = fetch(url)
+    k.then(response => {
+        return response.json();
+        
+    }).then(Image => {
+        const dogPicURL = document.createElement("section");
+        dogPicURL.id = "NewDogPic";
+        dogPic.append(dogPicURL);
+        dogPicURL.innerHTML = `${Image.image}`
+    })
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+//const k = fetch(url)
+//k.then(response => {
+//    return response.json();
+//}).then(Doggos => {
+//    DoggoDisplay(Doggos)
+//});
+
+//console.log("hit? 2")
+
+//function DoggoDisplay(dogs: any){
+  //  let button = document.querySelector("newDog") as HTMLElement;
+    //button.addEventListener("click", (event) => {
+        
+      //  })
+   // })
+//}
