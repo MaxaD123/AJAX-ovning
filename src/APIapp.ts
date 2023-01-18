5a6fbe24884b6985dbf86dff888831f88e2308f3
Max Taylor <maxtaylor0804@gmail.com>
2023-01-18 04:42:25 +0100

    liten uppdatering

--- src/APIapp.ts
+++ src/APIapp.ts
@@ -1,34 +1,42 @@
-const url = "https://dog.ceo/api/breeds/image/random"
-const dogPic = document.querySelector(".dogPic") as HTMLElement;
+//const url = "https://dog.ceo/api/breeds/image/random"
+//const dogPic = document.querySelector(".dogPic") as HTMLElement;
 
-let button = document.querySelector(".newDog") as HTMLElement;
-button.addEventListener("click", (event) => {
-    event.preventDefault();
-    dogPic.innerHTML = "";
-    
-    
-    const k = fetch(url)
-    k.then(response => {
-        return response.json();
-        
-    }).then(data => {
-        const dogPicURL = document.createElement("section");
-        dogPicURL.id = "NewDogPic";
-        dogPic.append(dogPicURL);
-        dogPicURL.innerHTML = `<img src="${data.image}" alt="">`
+async function dogs() {
+    const response = await fetch(`https://dog.ceo/api/breeds/image/random`);
+    const data = await response.json();
+
+    let dogPiCCes = document.querySelector('.dogPic') as HTMLElement;
+    dogPiCCes.innerHTML = ""
+    data.data.map((Obj: any) => {
+      dogPiCCes.innerHTML+= `<div><img src="${Obj.image}" alt="">`
     })
+}
+
+
+
+
+
+
+
+//let button = document.querySelector(".newDog") as HTMLElement;
+//button.addEventListener("click", (event) => {
+  //  event.preventDefault();
+    //dogPic.innerHTML = "";
     
-});
-
-
-
-
-
-
-
-
-
-
+    
+    //const k = fetch(url)
+//    k.then(response => {
+  //      return response.json();
+        
+//    })
+ //   k.then.(piccen => {
+   //     const dogPicURL = document.createElement("img");
+     //   dogPicURL.id = "NewDogPic";
+       // dogPic.append(dogPicURL);
+       // dogPicURL.innerHTML = `<img src="${piccen.image}" alt="">`;
+   // })
+    
+//});
 
 //const k = fetch(url)
 //k.then(response => {
