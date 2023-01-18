fae2de836943459eed371e4fc1d864b2d80e0dca
Max Taylor <maxtaylor0804@gmail.com>
2023-01-18 11:01:07 +0100

    Nu ska det verkligen funka

--- src/APIapp.ts
+++ src/APIapp.ts
@@ -1,35 +1,74 @@
-import axios from 'axios';
-
-      async function getRandomDogImages() {
-        try {
-          const promises = [1, 2, 3].map(async (num) => {
-            const response = await axios.get('https://dog.ceo/api/breeds/image/random%27');
-            return response.data.message;
-          });
-          const imageUrls = await Promise.all(promises);
-          const dogImages = [
-            document.getElementById('dog-image-1'),
-            document.getElementById('dog-image-2'),
-            document.getElementById('dog-image-3'),
-          ];
-          dogImages.forEach((img, index) => {
-            img.src = imageUrls[index];
-          });
-        } catch (error) {
-          console.error(error);
-        }
-      }
-
-      const newImagesButton = document.getElementById('new-images-button');
-      newImagesButton.addEventListener('click', getRandomDogImages);
-
-      // Call the function to get the first images on page load
-      getRandomDogImages();
 
 
+const cardsContainer = document.getElementById("cardsContainer")!;
+const newDogsButton = document.getElementById("newDogsButton")!;
+
+async function getRandomDogPhotos() {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random/3');
+    const data = await response.json();
+    return data.message;
+}
+
+function createDogCard(url: string) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const img = document.createElement("img");
+    img.src = url;
+    img.alt = "A cute dog";
+
+    card.appendChild(img);
+    return card;
+}
+
+async function showDogs() {
+    cardsContainer.innerHTML = "";
+    const dogUrls = await getRandomDogPhotos();
+    dogUrls.forEach((url: string) => {
+        const card = createDogCard(url);
+        cardsContainer.appendChild(card);
+    });
+}
+
+newDogsButton.addEventListener("click", showDogs);
+showDogs();
 
 
-      
+
+
+
+// import axios from 'axios';
+
+//       async function getRandomDogImages() {
+//         try {
+//           const promises = [1, 2, 3].map(async () => {
+//             const response = await axios.get('https://dog.ceo/api/breeds/image/random');
+//             return response.data.message;
+//           });
+//           const imageUrls = await Promise.all(promises);
+//           const dogImages = [
+//             document.getElementById('.dog-image-1'),
+//             document.getElementById('.dog-image-2'),
+//             document.getElementById('.dog-image-3'),
+//           ];
+//           dogImages.forEach((img, index) => {
+//             img.src = imageUrls[index];
+//           });
+//         } catch (error) {
+//           console.error(error);
+//         }
+//       }
+
+//       const newImagesButton = document.getElementById('.new-images-button');
+//       newImagesButton.addEventListener('click', getRandomDogImages);
+
+//       // Call the function to get the first images on page load
+//       getRandomDogImages();
+
+
+
+
+
 
 
 
