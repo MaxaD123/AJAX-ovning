a8581959ed06e95a65708b9af8c87853cddee495
Max Taylor <maxtaylor0804@gmail.com>
2023-01-18 10:26:40 +0100

    Nu ska skiten fungera

--- src/APIapp.ts
+++ src/APIapp.ts
@@ -1,19 +1,66 @@
+import axios from 'axios';
+
+      async function getRandomDogImages() {
+        try {
+          const promises = [1, 2, 3].map(async (num) => {
+            const response = await axios.get('https://dog.ceo/api/breeds/image/random%27');
+            return response.data.message;
+          });
+          const imageUrls = await Promise.all(promises);
+          const dogImages = [
+            document.getElementById('dog-image-1'),
+            document.getElementById('dog-image-2'),
+            document.getElementById('dog-image-3'),
+          ];
+          dogImages.forEach((img, index) => {
+            img.src = imageUrls[index];
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+      const newImagesButton = document.getElementById('new-images-button');
+      newImagesButton.addEventListener('click', getRandomDogImages);
+
+      // Call the function to get the first images on page load
+      getRandomDogImages();
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //const url = "https://dog.ceo/api/breeds/image/random"
-//const dogPic = document.querySelector(".dogPic") as HTMLElement;
+// const dogBtn = document.querySelector(".newDog") as HTMLElement;
+// dogBtn.addEventListener('click') => {
+// async function dogs() {
+//     const response = await fetch(`https://dog.ceo/api/breeds/image/random`);
+//     const data = await response.json();
 
-async function dogs() {
-    const response = await fetch(`https://dog.ceo/api/breeds/image/random`);
-    const data = await response.json();
-
-    let dogPiCCes = document.querySelector('.dogPic') as HTMLElement;
-    dogPiCCes.innerHTML = ""
-    data.data.image((Obj: any) => {
-      dogPiCCes.innerHTML+= `<div><img src="${Obj.image}" alt="">`
-    })
-    return data;
-}
-
-dogs();
+//     let dogPiCCes = document.querySelector('.dogPic') as HTMLElement;
+//     dogPiCCes.innerHTML = ""
+//     data.data.image((Obj: any) => {
+//       dogPiCCes.innerHTML+= `<div><img src="${Obj.image}" alt="">`
+//     })
+//     return data;
+// }}
 
 
 
